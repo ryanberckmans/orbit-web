2f92a958d98a7c1c2df151339be3852545b69650
Ryan Berckmans <ryanberckmans@gmail.com>
2020-11-01 11:35:14 -0600

    Set webpack publicPath to /orbit-web/ to fix x-origin iframe issue
    orbit-web can't be used in a cross-origin iframe: some browsers (such as Brave
    throw on IndexedDB.open, "OpenError: The user denied permission to access the
    database.", for which I haven't discovered a workaround.
    
    Instead, orbit-web should be served from the same origin as the app using it
    for embedded chat, as follows:
    
    1. orbit-web's webpack.config.js has been updated with `publicPath: '/orbit-web/',`
    2. orbit-web's production build in dist/ should be copied to /public/orbit-web in the client app
    3. the client app should load the iframe normally, from the same origin
    	eg. <iframe className="chatIframe" src="/orbit-web" />
    4. ensure the client app's linter is ignoring "public/orbit-web/**/*.js",

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const config = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: './',
+    publicPath: '/orbit-web/',
     filename: '[name].[hash:20].js',
     globalObject: 'this'
   },
